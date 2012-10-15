c5cb260de6385f74950896d9b1c9f82d9edd5081
yon <Lighthawky@gmail.com>
2012-10-15 11:40:28 +0200

    new site for 2012

--- app.rb
+++ app.rb
@@ -2,7 +2,15 @@ require 'sinatra'
 set :haml, :format => :html5
 
 get '/' do
-  haml :index
+  redirect "2012"
+end
+
+get '/2012' do
+  haml :day2012
+end
+
+get '/2011' do
+  haml :day2011
 end
 
 get '/style' do
