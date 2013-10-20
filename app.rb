6341d5e9153b674d68a4048a5bafa33a523e4f57
yon <Lighthawky@gmail.com>
2013-10-20 11:28:39 +0200

    added liveperson raanana

--- app.rb
+++ app.rb
@@ -9,12 +9,16 @@ get '/2013' do
   haml :day2013
 end
 
+get '/2013-raanana' do
+  haml :raanana2013
+end
+
 get '/2012' do
   haml :day2012
 end
 
-get '/haifa' do
-  haml :haifa
+get '/2012-haifa' do
+  haml :haifa2012
 end
 
 get '/2011' do
